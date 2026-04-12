f272dee44c241ac4ebb623e5f079ec280802f398
NavcodeTech <77974772+NavcodeTech@users.noreply.github.com>
2026-04-12 20:55:55 +0530

    First commit of aws

---
+++ src/main/java/com/awsApiGateway/exampProject/ExampProjectApplication.java
@@ -0,0 +1,39 @@
+package com.awsApiGateway.exampProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/book")
+public class ExampProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampProjectApplication.class, args);
+	}
+	
+	private List<Book> books = new ArrayList<>();
+	
+	
+	@PostMapping("/addBook")
+	public Book addBook(@RequestBody Book book) {
+		books.add(book);
+		return book;
+	}
+	
+	@GetMapping("/getBooks")
+	public List<Book> getBookDetails() {
+		return books;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/awsApiGateway/exampProject/ExampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsApiGateway.exampProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
